089a6f3c7c950661ab737b041f5ffbe5a05899de
jagadeeshvelmurugan <jagadeesh.velmurugan@gmail.com>
2026-09-22 15:06:53 +0530

    bulky json sb project initial stub

---
+++ bulky-json-demo/src/main/java/bulkyjson/bulky_json_demo/BulkyJsonDemoApplication.java
@@ -0,0 +1,13 @@
+package bulkyjson.bulky_json_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkyJsonDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkyJsonDemoApplication.class, args);
+	}
+
+}
---
+++ bulky-json-demo/src/test/java/bulkyjson/bulky_json_demo/BulkyJsonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bulkyjson.bulky_json_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkyJsonDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
